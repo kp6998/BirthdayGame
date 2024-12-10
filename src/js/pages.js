9a8bd4fe512339d951842ad0057833811d0e9328
prakash-esf <Prakash.Kathiresan@esolutionsfirst.com>
2024-12-10 23:04:12 +0530

    changes

--- src/js/pages.js
+++ src/js/pages.js
@@ -1,20 +1,21 @@
 export const soon = {
   title: "Come Back Later...",
   body: `<br />
-    <h1>Hi, you come to early</h1>
+    <h1 style="font-size: 30px;">Hi, you come to early</h1>
     <br /><br /><br />
-    <p>
-        I know this page is very interesting for you, especially for your special day but.<br />
-        You need to be patience until the time has come, right ?
+    <p style="font-size: 20px;">
+      I know this game is very exciting for you, especially on your special day.<br />
+      But you need to be patient until the time comes, right?<br />
+      So, wait until your birthday starts! 😊✨
     </p>`,
 };
 
 export const late = {
   title: "See you next time...",
   body: `<br />
-    <h1>The party was over</h1>
+    <h1 style="font-size: 30px;">The party was over</h1>
     <br /><br /><br />
-    <p>
+    <p style="font-size: 20px;">
         Yes, my gift for you is kinda simple, cheap, and weird ? &#128534<br>
         B-but. It's only for you. &#128150
     </p>
